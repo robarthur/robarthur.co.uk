3be9dba8f89844f388afe24077a566ddf7c2c329
Ubuntu <ubuntu@ip-172-31-14-202.eu-west-2.compute.internal>
2017-09-11 15:13:27 +0000

    Initial Commit

---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Rob Arthur'
+COPYRIGHT_NAME = 'Rob Arthur'
+SITEURL = 'robarthur.co.uk'
+SITENAME = 'Rob Arthur'
+SITETITLE = 'Rob Arthur'
+SITESUBTITLE = 'Personal Blog for notes, thoughts and experiments'
+SITEDESCRIPTION = 'Personal Blog of Rob Arthur, interested in software development, devops, AWS/Cloud.'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ( )
+
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+THEME = 'themes/flex'
+COPYRIGHT_YEAR = 2017
